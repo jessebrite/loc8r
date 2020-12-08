1c16b260fdfc147e00a691a6c04da2686e9a7e4c
jessebrite <jessebrite@gmail.com>
2020-12-08 00:24:06 +0000

    unnecessary code deleted

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -4,12 +4,3 @@ module.exports.index = (req, res) => {
 	res.render('index', {title : 'Express'});
 }
 
-module.exports.kankan = (req, res) => {
-  come make we do deh ting jeh jeh
-  but dem no dey barb everything man dem a seh
-}
-
-/*
- * Shwo dem clear pepper before they realize dem no dey kill vim
- * Always tryiing ro overdo things. Such a rerrble brethren
- **/
