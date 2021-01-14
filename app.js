38d9906d9c87d20517b292da053a6cb92ceed794
jessebrite <aboagye@protonmail.com>
2021-01-14 19:46:10 +0000

    set CORS back to 4200

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use('/api', (req, res, next) => {
   //      res.setHeader('Access-Control-Allow-Origin', origin);
   // }
   res
-    // .header('Access-Control-Allow-Origin', 'http://localhost:4200')
+    .header('Access-Control-Allow-Origin', 'http://localhost:4201')
     .header('Access-Control-Allow-Methods', '*')
     .header(
       'Access-Control-Allow-Headers',
